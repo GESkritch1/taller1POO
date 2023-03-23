ba440a668e2176fe6d16ac83cd15bc12e0764329
GESkritch1 <m.carrillo07@ufromail.cl>
2023-03-23 10:16:57 -0300

    commit3

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,77 @@
-import java.awt.event.ItemEvent;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     static Scanner teclado = new Scanner(System.in);
 
     private static void menu() {
-        System.out.println("elija 1 opcion del menu: ");
-        int OpcionIngresada = teclado.nextInt();
+        boolean salir = true;
+        do {
 
-        switch (OpcionIngresada) {
-            case 1:
-                System.out.println("Opcion 1: Ingresar datos");
-                ingresoDeDatos();
-                break;
-            case 2:
-                System.out.println("Opcion 2: Mostrar sismos de mayor magnitud");
-                sismosMayorMagnitud();
-                break;
-            case 3:
-                System.out.println("Opcion 3: Contar sismos mayores o igual a 5.0");
-                sismosMayoresQue5();
-                break;
-            case 4:
-                System.out.println("Opcion 4 Enviar SMS por cada sismo mayor o igual a 7.0");
-                enviarSMSPorCadaSismo();
-                break;
-            case 5:
-                System.out.println("Salir del programa");
-                break;
-            default:
-        }
+            System.out.println("elija 1 opcion del menu con un numero: ");
+            System.out.println("Opcion 1: Ingresar datos");
+            System.out.println("Opcion 2: Mostrar sismos de mayor magnitud");
+            System.out.println("Opcion 3: Contar sismos mayores o igual a 5.0");
+            System.out.println("Opcion 4: Enviar SMS por cada sismo mayor o igual a 7.0");
+            System.out.println("Opcion 5:Salir del programa");
+            int OpcionIngresada = teclado.nextInt();
+
+            switch (OpcionIngresada) {
+                case 1:
+                    ingresoDeDatos();
+                    break;
+                case 2:
+                    sismosMayorMagnitud();
+                    break;
+                case 3:
+                    sismosMayoresQue5();
+                    break;
+                case 4:
+                    enviarSMSPorCadaSismo();
+                    break;
+                case 5:
+                    salir=false;
+                    break;
+                default:
+                    System.out.println("ese dato no existe ingrese otra vez");
+                    break;
+            }
+        }while (salir == true);
+    }
+    private static void enviarSMSPorCadaSismo() {
+
     }
 
-    public static void ingresoDeDatos() {
+    public static void sismosMayoresQue5() {
+
+    }
+
+    private static void sismosMayorMagnitud() {
+
+    }
+
+    public static double[] ingresoDeDatos() {
+
+        double[] datos = new double[168];
+        Random  rnd = new Random();
+
+
+        for (int i = 0; i<168; i++){
+
+            datos[i] = Math.floor(((rnd.nextDouble(0.0,9.9))*10)/10);
+        }
+
+        for (int i = 0;i<168; i++){
+            System.out.println(datos[i]);
+        }
+
+        return datos;
 
     }
 
 
     public static void main(String[] args) {
-        menu();
+
+            menu();
     }
 }
